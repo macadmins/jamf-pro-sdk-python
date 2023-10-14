5b50e69457b6558cffa8a98858e6b349d25f75f6
Tony Williams <honestpuck@gmail.com>
2023-10-14 12:54:41 +1100

    black formatted

--- tests/models/test_models_classic_categories.py
+++ tests/models/test_models_classic_categories.py
@@ -3,22 +3,18 @@ import json
 from deepdiff import DeepDiff
 from src.jamf_pro_sdk.models.classic.categories import ClassicCategory
 
-CATEGORY_JSON = {
-    "category": {
-        "id": 1,
-        "name": "Test Category",
-        "priority": 1
-    }
-}
+CATEGORY_JSON = {"category": {"id": 1, "name": "Test Category", "priority": 1}}
+
 
 def test_category_model_parsings():
     """Verify select attributes across the ComputerGroup model."""
     category = ClassicCategory(**CATEGORY_JSON["category"])
-    
+
     assert category is not None  # mypy
     assert category.name == "Test Category"
     assert category.priority == 1
     assert category.id == 1
+
 
 def test_category_model_json_output_matches_input():
     category = ClassicCategory(**CATEGORY_JSON["category"])
